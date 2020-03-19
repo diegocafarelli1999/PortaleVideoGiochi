590896d3990a86754b70ecb76bad2a6d3e1072ea
diegocafarelli <diegocafarelli9991@gmail.com>
2020-03-19 19:15:16 +0100

    creazione componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { GamesListComponent } from './component/games-list/games-list.component';
+import { GameDetailComponent } from './component/game-detail/game-detail.component';
+import { EditGameComponent } from './component/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    GamesListComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
